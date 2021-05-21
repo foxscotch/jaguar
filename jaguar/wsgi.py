55ba0580bc495ff5398b9c6dea33d9a52602b3b5
foxscotch <foxscotch@gmail.com>
2021-05-21 00:07:50 -0500

    Use Black formatting

--- jaguar/wsgi.py
+++ jaguar/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jaguar.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jaguar.settings")
 
 application = get_wsgi_application()
--- upce.py
+++ upce.py
@@ -7,28 +7,32 @@ def expand(upce: str):
     """Expand a UPC-E number to its proper full-length UPC-A."""
 
     digits = list(upce)
-    number_system = digits.pop(0) if (len(digits) > 7) else ''
-    check_digit = digits.pop() if (len(digits) > 6) else ''
+    number_system = digits.pop(0) if (len(digits) > 7) else ""
+    check_digit = digits.pop() if (len(digits) > 6) else ""
     last_digit = digits.pop()
 
     expanded = None
 
-    if last_digit in ('0', '1', '2'):
-        expanded = ''.join(digits[0:2]) + last_digit + '0000' + ''.join(digits[2:5])
-    elif last_digit in ('3', '4'):
-        expanded = ''.join(digits[0:last_digit]) + '00000' + ''.join(digits[last_digit:5])
-    elif last_digit in ('5', '6', '7', '8', '9'):
-        expanded = ''.join(digits) + '0000' + last_digit
+    if last_digit in ("0", "1", "2"):
+        expanded = "".join(digits[0:2]) + last_digit + "0000" + "".join(digits[2:5])
+    elif last_digit in ("3", "4"):
+        expanded = (
+            "".join(digits[0:last_digit]) + "00000" + "".join(digits[last_digit:5])
+        )
+    elif last_digit in ("5", "6", "7", "8", "9"):
+        expanded = "".join(digits) + "0000" + last_digit
 
     return number_system + expanded + check_digit
+
 
 def compress(upce: str):
     """TODO?: Expand a UPC-A number to its shortened UPC-E, if possible."""
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    expanded = expand(sys.argv[1] if len(sys.argv) > 1 else '07831504')
+
+    expanded = expand(sys.argv[1] if len(sys.argv) > 1 else "07831504")
     print(expanded)
     print(compress(expanded))
