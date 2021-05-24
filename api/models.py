29183984a9d1145eea4ca849e8e045ff85cfe427
foxscotch <foxscotch@gmail.com>
2021-05-24 01:39:26 -0500

    Add a few food tracking models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class FoodItem(models.Model):
+    name = models.CharField()
+    brand = models.ForeignKey("Brand", models.CASCADE)
+    cost = models.IntegerField()
+    gtin = models.CharField(max_length=14)
+
+    serving_size = models.FloatField()
+    serving_unit = models.CharField(max_length=16)
+    serving_size_alt = models.CharField(
+        max_length=32, help_text="Alternate human-readable serving size, e.g. '1 box'"
+    )
+    calories = models.IntegerField()
+    nutrition = models.ManyToManyField("NutritionInfo")
+
+
+class NutritionInfo(models.Model):
+    field = models.ForeignKey("NutritionField", models.PROTECT)
+    value = models.FloatField()
+
+
+class NutritionField(models.Model):
+    name = models.CharField(max_length=32)
+    unit = models.CharField(max_length=16)
