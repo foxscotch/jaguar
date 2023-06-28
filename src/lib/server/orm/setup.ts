6348c336ef54e518ac59cc576d05287e8ad19add
Michel Passos <michelfariarj@gmail.com>
2023-06-27 19:01:57 -0500

    JAG-10 Add TypeORM to the project

---
+++ src/lib/server/orm/setup.ts
@@ -0,0 +1,14 @@
+
+import { AppDataSource } from "./data-source";
+
+export async function initDataSource() {
+    try {
+        await AppDataSource.initialize();
+        console.log("Connected to database");
+    } catch (err) {
+        throw new Error(
+            "Error occurred while connecting to database",
+            { cause: err }
+        );
+    }
+}
