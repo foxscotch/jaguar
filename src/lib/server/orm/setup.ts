692c010cb447f862da2fa09fd7933bfb4db93c12
Foxscotch <foxscotch@gmail.com>
2023-11-07 01:11:08 -0600

    Prevent initDataSource() from throwing if already run

--- src/lib/server/orm/setup.ts
+++ src/lib/server/orm/setup.ts
@@ -1,6 +1,8 @@
 import { AppDataSource } from "./data-source";
 
 export async function initDataSource() {
+  if (AppDataSource.isInitialized) return;
+
   try {
     await AppDataSource.initialize();
     console.log("Connected to database");
